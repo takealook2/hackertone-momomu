038cc0130ff8f32161477cd1d9a03d5d2aa5792a
takealook2 <a1vm5h@gmail.com>
2021-08-05 00:39:13 +0900

    현재 오류: 부트스트랩 적용 안 됨

--- member/urls.py
+++ member/urls.py
@@ -1,8 +1,7 @@
-from board.views import register
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('register/', views.register),
-    path('', views.register),
+    path('',views.register),
 ]
